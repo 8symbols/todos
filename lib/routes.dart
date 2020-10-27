0c27556cb1b2ce8667262af45ca4a597d865b86a
8symbols <ivan991015@gmail.com>
2020-10-27 19:14:07 +1000

    Переименовал файл TodosListScreen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todos/data/repositories/todos_repository/repositories/mock_todos_repository.dart';
-import 'package:todos/presentation/todos_list/TodosListScreen.dart';
+import 'package:todos/presentation/todos_list/todos_list_screen.dart';
 
 const initialRoute = TodosListScreen.routeName;
 
