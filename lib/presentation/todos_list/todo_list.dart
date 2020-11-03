71d76427dad0ac7fc6601181196899ad221c9a80
8symbols <ivan991015@gmail.com>
2020-11-03 10:52:29 +1000

    Изменил дизайн списка задач

--- lib/presentation/todos_list/todo_list.dart
+++ lib/presentation/todos_list/todo_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:todos/domain/models/todo.dart';
 import 'package:todos/presentation/todos_list/bloc/todo_list_bloc.dart';
 import 'package:todos/presentation/todos_list/todo_card.dart';
@@ -16,18 +17,23 @@ class TodoList extends StatelessWidget {
     const emptySpaceForFabHeight = 88.0;
 
     return todos.isEmpty
-        ? const Center(child: Text('Нет элементов'))
-        : ListView(
+        ? _buildEmptyList()
+        : Stack(
             children: [
-              ...todos
-                  .map((todo) => TodoCard(
-                        todo,
-                        onDelete: () => _deleteTodo(context, todo),
-                        onEdit: (editedTodo) =>
-                            _editTodo(context, todo.id, editedTodo),
-                      ))
-                  .toList(),
-              const SizedBox(height: emptySpaceForFabHeight),
+              _buildBackgroundLines(context),
+              ListView(
+                children: [
+                  ...todos
+                      .map((todo) => TodoCard(
+                            todo,
+                            onDelete: () => _deleteTodo(context, todo),
+                            onEdit: (editedTodo) =>
+                                _editTodo(context, todo.id, editedTodo),
+                          ))
+                      .toList(),
+                  const SizedBox(height: emptySpaceForFabHeight),
+                ],
+              ),
             ],
           );
   }
@@ -51,5 +57,53 @@ class TodoList extends StatelessWidget {
 
   void _editTodo(BuildContext context, String todoId, Todo editedTodo) {
     context.bloc<TodoListBloc>().add(TodoEditedEvent(editedTodo));
+  }
+
+  Widget _buildEmptyList() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SvgPicture.asset(
+            'assets/images/todolist.svg',
+            width: 200.0,
+            height: 200.0,
+          ),
+          const SizedBox(height: 16.0),
+          ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 150.0),
+            child: const Text(
+              'На данный момент задачи отсутствуют',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 18.0),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBackgroundLines(BuildContext context) {
+    const backgroundLineThickness = 1.0;
+    const backgroundLinesDistance = 70.0;
+
+    const backgroundLine = Padding(
+      padding: EdgeInsets.only(top: backgroundLinesDistance),
+      child: Divider(
+        height: backgroundLineThickness,
+        indent: 20.0,
+        endIndent: 20.0,
+        color: Colors.grey,
+        thickness: backgroundLineThickness,
+      ),
+    );
+
+    final backgroundLinesCount = MediaQuery.of(context).size.height ~/
+            (backgroundLinesDistance + backgroundLineThickness) +
+        1;
+
+    return Wrap(
+      children: [for (var i = 0; i < backgroundLinesCount; ++i) backgroundLine],
+    );
   }
 }
