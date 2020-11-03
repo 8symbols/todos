f6e09bf1543f6d7aad589b8f9663586bc990df2f
Ivan Vasilenko <ivan991015@gmail.com>
2020-11-03 21:44:17 +1000

    Изменил цвета тем

--- lib/presentation/branch_themes.dart
+++ lib/presentation/branch_themes.dart
@@ -6,7 +6,7 @@ const defaultBranchTheme = BranchTheme(Color(0xFF6202EE), Color(0xFFB5C9FD));
 
 /// Доступные темы веток.
 const branchThemes = [
-  BranchTheme(Color(0xFFD50000), Color(0xFFFF8A80)),
+  BranchTheme(Color(0xFFD50000), Color(0xFFEF5350)),
   BranchTheme(Color(0xFFEF6C00), Color(0xFFFFCC80)),
   BranchTheme(Color(0xFFFDD835), Color(0xFFFFF59D)),
   BranchTheme(Color(0xFF388E3C), Color(0xFFA5D6A7)),
