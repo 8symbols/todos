5a14bc24988141b9e154fcc0e8927444b319c35c
Ivan Vasilenko <ivan991015@gmail.com>
2020-11-18 17:17:29 +1000

    Переименовал ассеты

--- lib/presentation/constants/assets_paths.dart
+++ lib/presentation/constants/assets_paths.dart
@@ -1,8 +1,8 @@
 /// Пути к ассетам.
 abstract class AssetsPaths {
   /// Placeholder на время загрузки картинки.
-  static const imagePlaceholder = 'assets/images/placeholder.jpg';
+  static const imagePlaceholder = 'assets/images/image_placeholder.jpg';
 
   /// Изображение не доступно.
-  static const imageNotAvailable = 'assets/images/error.png';
+  static const imageNotAvailable = 'assets/images/image_not_available.png';
 }
