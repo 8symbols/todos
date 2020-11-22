0436c33f4d2a94e8ccfa37e06c71701d2968400b
Ivan Vasilenko <ivan991015@gmail.com>
2020-11-22 14:20:17 +1000

    Убрал лишние поля

--- lib/domain/models/todo.dart
+++ lib/domain/models/todo.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:todos/domain/models/todo_priority.dart';
 import 'package:todos/domain/wrappers/nullable.dart';
 import 'package:uuid/uuid.dart';
 
@@ -35,9 +34,6 @@ class Todo {
   /// Время создания задачи.
   final DateTime creationTime;
 
-  /// Приоритет задачи.
-  final TodoPriority priority;
-
   /// Путь к фотографии, которая устанавливается в [AppBar] на экране задачи.
   ///
   /// Может быть равным null. В этом случае [AppBar] окрашивается в цвет ветки
@@ -54,7 +50,6 @@ class Todo {
     DateTime creationTime,
     this.isFavorite = false,
     this.wasCompleted = false,
-    this.priority = TodoPriority.medium,
     this.themeImagePath,
     this.note,
     this.deadlineTime,
@@ -63,15 +58,13 @@ class Todo {
         creationTime = creationTime ?? DateTime.now(),
         assert(title != null),
         assert(isFavorite != null),
-        assert(wasCompleted != null),
-        assert(priority != null);
+        assert(wasCompleted != null);
 
   Todo copyWith({
     String id,
     bool isFavorite,
     bool wasCompleted,
     DateTime creationTime,
-    TodoPriority priority,
     Nullable<String> themeImagePath,
     Nullable<String> note,
     Nullable<DateTime> deadlineTime,
@@ -84,7 +77,6 @@ class Todo {
       isFavorite: isFavorite ?? this.isFavorite,
       wasCompleted: wasCompleted ?? this.wasCompleted,
       creationTime: creationTime ?? this.creationTime,
-      priority: priority ?? this.priority,
       themeImagePath:
           themeImagePath != null ? themeImagePath.value : this.themeImagePath,
       note: note != null ? note.value : this.note,
